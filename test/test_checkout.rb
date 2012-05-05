def480f82629558477737aaa81286d811245d5d2
John Nolan <johnnonolan@yahoo.co.uk>
2012-05-05 23:14:06 +0100

    fixed it, yay!

--- test/test_checkout.rb
+++ test/test_checkout.rb
@@ -3,7 +3,7 @@ require "test/unit"
 class TestPrice < Test::Unit::TestCase
    
     PRICES = {"A"=> 50, "B" => 30, "C" => 20, "D" => 15}
-    DISCOUNTS = {"A" => {3 => 20}} 
+    DISCOUNTS = {"A" => {3 => 20},"B" => {2 => 15}} 
     def price(goods)
 		co = CheckOut.new(PRICES,DISCOUNTS)
 		goods.split(//).each { |item| co.scan(item) }
@@ -11,17 +11,17 @@ class TestPrice < Test::Unit::TestCase
     end
 
     def test_totals
-#		assert_equal( 0, price("")) 
-#		assert_equal( 50, price("A"))
-#		assert_equal( 80, price("AB"))
-#		assert_equal(115, price("CDBA"))
-#		assert_equal(100, price("AA"))	
-#		assert_equal(130, price("AAA"))
-#		assert_equal(180, price("AAAA"))
-#		assert_equal(230, price("AAAAA"))
-#		assert_equal(260, price("AAAAAA"))
+		assert_equal( 0, price("")) 
+		assert_equal( 50, price("A"))
+		assert_equal( 80, price("AB"))
+		assert_equal(115, price("CDBA"))
+		assert_equal(100, price("AA"))	
+		assert_equal(130, price("AAA"))
+		assert_equal(180, price("AAAA"))
+		assert_equal(230, price("AAAAA"))
+		assert_equal(260, price("AAAAAA"))
 		
-#		assert_equal(160, price("AAAB"))
+		assert_equal(160, price("AAAB"))
 		assert_equal(175, price("AAABB"))
     end
 
